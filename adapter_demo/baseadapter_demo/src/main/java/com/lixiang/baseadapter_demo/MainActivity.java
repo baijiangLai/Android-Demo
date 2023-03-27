a6b020e6f5ce13accec29f74e8c33ca6300dc754
baijiangLai <13198222888@163.com>
2023-03-27 11:29:01 +0800

    Division line commit

--- adapter_demo/baseadapter_demo/src/main/java/com/lixiang/baseadapter_demo/MainActivity.java
+++ adapter_demo/baseadapter_demo/src/main/java/com/lixiang/baseadapter_demo/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.lixiang.baseadapter_demo.adapter.AnimalAdapter;
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private AnimalAdapter mAdapter = null;
     private ListView list_animal;
 
+    private LinearLayout ly_content;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +32,22 @@ public class MainActivity extends AppCompatActivity {
         mContext = MainActivity.this;
         list_animal = (ListView) findViewById(R.id.list_animal);
 
+        //动态加载顶部View和底部View
+        final LayoutInflater inflater = LayoutInflater.from(this);
+        View headView = inflater.inflate(R.layout.view_header,null,false);
+        View footView = inflater.inflate(R.layout.view_footer, null, false);
+
+
         mData = new LinkedList<>();
         mData.add(new Animal("狗说", "你是狗么?", R.mipmap.ic_launcher));
         mData.add(new Animal("牛说", "你是牛么?", R.mipmap.ic_launcher));
         mData.add(new Animal("鸭说", "你是鸭么?", R.mipmap.ic_launcher));
         mData.add(new Animal("鱼说", "你是鱼么?", R.mipmap.ic_launcher));
         mData.add(new Animal("马说", "你是马么?", R.mipmap.ic_launcher));
+
+        //添加表头和表尾需要写在setAdapter方法调用之前！！！
+        list_animal.addHeaderView(headView);
+        list_animal.addFooterView(footView);
 
         mAdapter = new AnimalAdapter(mContext, (LinkedList<Animal>) mData);
         list_animal.setAdapter(mAdapter);
